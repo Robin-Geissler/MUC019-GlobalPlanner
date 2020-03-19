41aa5926860c716786020a14598ef8de53a235c3
Thilo Mueller <t.mueller@tufast.de>
2020-03-19 19:47:31 +0100

    added all inputs

--- include/global_planner_node.hpp
+++ include/global_planner_node.hpp
@@ -6,38 +6,46 @@
 #include <tufast_msgs/Lines.h>
 #include <tufast_msgs/PerceptionPoints.h>
 #include <tufast_msgs/StatusMessage.h>
+#include <tufast_msgs/MissionStatus.h>
 
 #include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/Path.h>
 
-#include <tf2_ros/transform_listener.h>
+#include <jsk_recognition_msgs/BoundingBoxArray.h>
 
-#include "global_planner.hpp"
+#include <tf2_ros/transform_listener.h>
 
 namespace tufast_planner {
 
-class GlobalPlanner;
+typedef enum e_MissionType {
+    TRACK_DRIVE = 1,
+    PARKING     = 2,
+    SLALOM      = 3,
+    ROLLOUT     = 100
+} MissionType;
 
 class GlobalPlannerNode {
   private:
-    ros::Subscriber _subControlStatus;
+    ros::Subscriber _subBoundingBoxes;
+    ros::Subscriber _subOccupancyGridMap;
+    ros::Subscriber _subMissionStatus;
 
     tf2_ros::Buffer            _tfBuffer;
     tf2_ros::TransformListener _tfListener;
 
     ros::Publisher _pubGoalPoses;
     ros::Publisher _pubPath;
-    ros::Publisher _pubStatus;
 
-    geometry_msgs::Pose _basePosition;
-
-    tufast_msgs::ControlStatusMessage _old_control_status_messsage;
-
-    GlobalPlanner _gp();
+    geometry_msgs::Pose _currentPosition;
+    MissionType         _currentMission;
 
   public:
     GlobalPlannerNode(ros::NodeHandle nh);
     ~GlobalPlannerNode();
+
+    void boundingBoxCallback(const jsk_recognition_msgs::BoundingBoxArray& msg);
+    void occuGridMapCallback(const nav_msgs::OccupancyGrid& msg);
+    void missionStatusCallback(const tufast_msgs::MissionStatus& msg);
 };
 
 } // namespace tufast_planner
--- src/global_planner_node.cpp
+++ src/global_planner_node.cpp
@@ -1,6 +1,5 @@
 #include <ros/ros.h>
 #include "global_planner_node.hpp"
-#include "global_planner.hpp"
 
 using namespace tufast_planner;
 
@@ -10,8 +9,11 @@ int main(int argc, char *argv[]) {
 
   GlobalPlannerNode node(nodeHandle);
 
+  
+
   ros::spin();
 }
+
 
 GlobalPlannerNode::GlobalPlannerNode(ros::NodeHandle nh) : _tfListener{_tfBuffer} {
     
@@ -19,14 +21,12 @@ GlobalPlannerNode::GlobalPlannerNode(ros::NodeHandle nh) : _tfListener{_tfBuffer
     // subscribers
     // ----------------------------------------
 
-    _subControlStatus = nh.subscribe<tufast_msgs::ControlStatusMessage>(
-      "/rosbridge/control_status", 1,
-      [this](const tufast_msgs::ControlStatusMessageConstPtr&
-                 msg) {
-        ROS_INFO("Received a control status message");
-      });
+    _subBoundingBoxes = nh.subscribe("~bounding_boxes", 1000, &GlobalPlannerNode::boundingBoxCallback, this);
 
+    _subOccupancyGridMap = nh.subscribe("~map", 1000, &GlobalPlannerNode::occuGridMapCallback, this);
 
+    _subMissionStatus = nh.subscribe("~mission_status", 1000, &GlobalPlannerNode::missionStatusCallback, this);
+    
     // -----------------------------------------
     // transform listener
     // ----------------------------------------
@@ -34,11 +34,11 @@ GlobalPlannerNode::GlobalPlannerNode(ros::NodeHandle nh) : _tfListener{_tfBuffer
     geometry_msgs::TransformStamped t;
     try {
         t = _tfBuffer.lookupTransform("/map", "/base", ros::Time(0));
-        _basePosition.position.x = t.transform.translation.x;
-        _basePosition.position.y = t.transform.translation.y;
-        _basePosition.position.z = t.transform.translation.z;
+        _currentPosition.position.x = t.transform.translation.x;
+        _currentPosition.position.y = t.transform.translation.y;
+        _currentPosition.position.z = t.transform.translation.z;
         
-         _basePosition.orientation = t.transform.rotation;
+        _currentPosition.orientation = t.transform.rotation;
     } catch (tf2::TransformException& ex) {
         ROS_WARN("%s", ex.what());
         ros::Duration(1.0).sleep();
@@ -49,8 +49,21 @@ GlobalPlannerNode::GlobalPlannerNode(ros::NodeHandle nh) : _tfListener{_tfBuffer
     // ----------------------------------------
 
     _pubGoalPoses = nh.advertise<tufast_msgs::GoalPoints>("goal_poses", 1);
-    _pubStatus = nh.advertise<tufast_msgs::StatusMessage>("status", 1);
     _pubPath = nh.advertise<nav_msgs::Path>("path", 1);
 }
 
+
 GlobalPlannerNode::~GlobalPlannerNode() {}
+
+void GlobalPlannerNode::boundingBoxCallback(const jsk_recognition_msgs::BoundingBoxArray& msg) {
+    ROS_INFO("Received a bounding box");
+}
+
+void GlobalPlannerNode::occuGridMapCallback(const nav_msgs::OccupancyGrid& msg) {
+    ROS_INFO("Received a map");
+}
+
+void GlobalPlannerNode::missionStatusCallback(const tufast_msgs::MissionStatus& msg) {
+    ROS_INFO("Received a mission status");
+    _currentMission = (MissionType) msg.selectedMission;
+}
