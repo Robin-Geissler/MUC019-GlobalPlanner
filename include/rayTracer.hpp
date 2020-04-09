faba2c82074976b4e2e569ffc5130975a2df759d
Robin Geissler <geissler@tufast.de>
2020-04-09 12:52:10 +0200

    Adds new Architecture for Classes RayTracer and Ray

--- include/rayTracer.hpp
+++ include/rayTracer.hpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <cmath>
 
+#include <nav_msgs/OccupancyGrid.h>
+
 #define RAYTRACER_RAY_START_X 50
 #define RAYTRACER_RAY_START_Y 0
 #define RAYTRACER_RAYNUMBER 25  // should always be odd
@@ -32,6 +34,7 @@ class Ray{
 private:
     Vec2 start; // start point of the Ray
     Vec2 dir;   // direction of the Ray
+    std::vector<Vec2> occuGridFields; // the Ray fields in the occupancyGrid (Vec2 as x,y coordinate)
 
 
 
@@ -42,21 +45,27 @@ public:
     void setStart(const Vec2 &start);
     const Vec2 &getDir() const;
     void setDir(const Vec2 &dir);
+    void setOccuGridFields(nav_msgs::OccupancyGrid& inputGrid);
 
-    float getLength(int8_t occupancyGrid);
+    float getLength();
     Vec2 getCenter();
 };
 
 class RayTracer{
 private:
-    std::vector<Ray> rays;
+    std::vector<Ray> rays;  // holds the Rays of the RayTracer
+    nav_msgs::OccupancyGrid inputGrid;  // input Occupancy Grid to calculate Ray collisions
+    nav_msgs::OccupancyGrid outputGrid; // output Occupancy Grid where the Goal Point is set to 5
+
 
 public:
     explicit RayTracer(int numberOfVectors);
     const std::vector<Ray> &getRays() const;
-    void addRay(const Ray &ray);
-    void removeRay(int index);
     Ray getLongestRay();
+    const nav_msgs::OccupancyGrid &getInputGrid() const;
+    void setInputGrid(const nav_msgs::OccupancyGrid &inputGrid);
+    const nav_msgs::OccupancyGrid &getOutputGrid() const;
+    void setOutputGrid(const nav_msgs::OccupancyGrid &outputGrid);
 
 };
 #endif //GLOBAL_PLANNER_RAYTRACER_HPP
